25ad7e05d00449fd07627808da7c8ac341b5886f
MarynaIlchenko <debovamail@gmail.com>
2022-02-12 11:02:52 +0200

    css and fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import countryCardTemplate from '../src/templates/country-card.hbs';
 import countryListTemplate from '../src/templates/country-list.hbs';
 
 const DEBOUNCE_DELAY = 300;
+const inputRef = document.querySelector('input#search-box');
+const countryInfoRef = document.querySelector('div.country-info');
+const countryListRef = document.querySelector('ul.country-list');
+let dataInput = '';
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,13 +2,23 @@ import '../css/styles.css';
 
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-function fetchCountries(name) {
-  fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(response => {
-      return response.json();
-    })
-    .then(country => {
-      console.log(country);
-    })
-    .catch(error => [console.log(error)]);
+async function fetchCountries(name) {
+  const response = await fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`,
+  );
+  if (!response.ok) {
+    return response.json().then(error => Promise.reject(error));
+  }
+  return await response.json();
 }
+
+export default { fetchCountries };
+
+// export function fetchCountries(name) {
+//   return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
+//     if (!response.ok) {
+//       return response.json().then(error => Promise.reject(error));
+//     }
+//     return response.json();
+//   });
+// }
