49405212655a16ef83d28c29ff85f560debd2ea6
MarynaIlchenko <debovamail@gmail.com>
2022-02-13 00:01:34 +0200

    commit

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,11 +3,12 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 export function fetchCountries(name) {
-    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(response => {
-        if (!response.ok) {
-    return response.json().then(error => Promise.reject(error));
+  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`).then(
+    response => {
+      if (!response.ok) {
+        return response.json().then(error => Promise.reject(error));
       }
-    return response.json();
-    });
-  }
+      return response.json();
+    },
+  );
+}
